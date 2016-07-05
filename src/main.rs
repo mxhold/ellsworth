bc9dd356d9f393885d7c35d1018ee2625b7b1572
Max Holder <mxhold@gmail.com>
2016-07-05 16:30:03 -0400

    Generate randomly

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
 extern crate image;
+extern crate rand;
 
 use std::fs::File;
 use std::path::Path;
 
+
+
 fn main() {
     let image_size = 400;
-    let tiles_per_row = 4;
-    let tile_size = image_size / tiles_per_row;
+    let tiles_per_row: usize = 10;
+    let tile_size = image_size / tiles_per_row as u32;
 
     let mut imgbuf = image::ImageBuffer::new(image_size, image_size);
 
-    let red = [255, 0, 0];
+    let red: [u8; 3] = [255, 0, 0];
     let blue = [0, 255, 0];
     let green = [0, 0, 255];
     let black = [0, 0, 0];
 
-    let colors = [
-        [red, blue, green, black],
-        [blue, green, black, red],
-        [green, black, red, blue],
-        [black, red, blue, green],
-    ];
+    let colors = [red, blue, green, black];
+
+    let mut tiles = vec![vec![[0; 3]; tiles_per_row]; tiles_per_row];
+
+    let mut rng = rand::thread_rng();
+
+    for row in &mut tiles {
+        for mut tile in row {
+            *tile = *rand::sample(&mut rng, colors.iter(), 1)[0];
+        }
+    }
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let xx = x / tile_size;
         let yy = y / tile_size;
 
-        let rgb = colors[xx as usize][yy as usize];
+        let rgb = tiles[xx as usize][yy as usize];
 
         *pixel = image::Rgb(rgb);
     }
