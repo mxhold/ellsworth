9c5e11f920aa97e5af6751d975ee65f64b67324f
Max Holder <mxhold@gmail.com>
2016-07-24 10:28:30 -0700

    Add RGB struct

--- src/main.rs
+++ src/main.rs
@@ -6,49 +6,62 @@ use std::fs::File;
 use std::path::Path;
 use std::env;
 
-fn read_colors(filepath: &str) -> Vec<[u8; 3]> {
+#[derive(Clone)]
+struct RGB {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl RGB {
+    fn new(red: u8, green: u8, blue: u8) -> RGB {
+        RGB { red: red, green: green, blue: blue }
+    }
+}
+
+fn read_colors(filepath: &str) -> Vec<RGB> {
     let mut csv_reader = csv::Reader::from_file(filepath).unwrap().has_headers(true);
 
-    let mut colors: Vec<[u8; 3]> = vec![];
+    let mut colors: Vec<RGB> = vec![];
 
     for row in csv_reader.decode() {
         let (_name, red, green, blue): (String, u8, u8, u8) = row.unwrap();
-        colors.push([red, green, blue]);
+        colors.push(RGB::new(red, green, blue));
     }
 
     colors
 }
 
-fn generate_tiles(colors: Vec<[u8; 3]>, tiles_per_row: usize) -> Vec<Vec<[u8; 3]>> {
-    let mut tiles = vec![vec![[0; 3]; tiles_per_row]; tiles_per_row];
+fn generate_tiles(colors: Vec<RGB>, tiles_per_row: usize) -> Vec<Vec<RGB>> {
+    let mut tiles = vec![vec![RGB::new(0, 0, 0); tiles_per_row]; tiles_per_row];
 
     let mut rng = rand::thread_rng();
 
     for row in &mut tiles {
         for mut tile in row {
-            *tile = *rand::sample(&mut rng, colors.iter(), 1).pop().unwrap();
+            *tile = rand::sample(&mut rng, colors.iter(), 1).pop().unwrap().clone();
         }
     }
 
     tiles
 }
 
-fn generate_image(image_size: u32, tile_size: u32, tiles: Vec<Vec<[u8; 3]>>) -> image::RgbImage {
+fn generate_image(image_size: u32, tile_size: u32, tiles: Vec<Vec<RGB>>) -> image::RgbImage {
     let mut imgbuf: image::RgbImage = image::ImageBuffer::new(image_size, image_size);
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let xx = x / tile_size;
         let yy = y / tile_size;
 
-        let rgb = tiles[xx as usize][yy as usize];
+        let ref rgb = tiles[xx as usize][yy as usize];
 
-        *pixel = image::Rgb(rgb);
+        *pixel = image::Rgb([rgb.red, rgb.green, rgb.blue]);
     }
 
     imgbuf
 }
 
-fn write_image_file(image: image::RgbImage, filepath: &str) -> Result<(), image::ImageError> {
+fn save_image(image: image::RgbImage, filepath: &str) -> Result<(), image::ImageError> {
     let ref mut fout = File::create(&Path::new(filepath)).unwrap();
 
     image::ImageRgb8(image).save(fout, image::PNG)
@@ -67,7 +80,7 @@ fn create_image(size: u32,
 
     let image = generate_image(size, tile_size, tiles);
 
-    write_image_file(image, output_filepath)
+    save_image(image, output_filepath)
 }
 
 fn main() {
